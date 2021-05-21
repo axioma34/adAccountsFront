dc421778680226c51037246d2083c86c148f6447
rnt <andrew.rennit@gmail.com>
2021-05-21 17:22:13 +0700

    logs removed

--- src/router/index.js
+++ src/router/index.js
@@ -44,8 +44,6 @@ const router = new VueRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  console.log(to)
-  console.log(store.getters.isAuthenticated)
   if(to && to.path !== '/login') {
     if (store.getters.isAuthenticated) {
       next()
